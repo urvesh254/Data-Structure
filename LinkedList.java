79dce709ff28cf7435acdc7572585e035b2a652d
urvesh254 <urveshpatel2001@gmail.com>
2020-10-27 14:26:08 +0530

    Adding Generics in LinkedList.

--- LinkedList.java
+++ LinkedList.java
@@ -1,169 +1,188 @@
 import java.util.*;
 
-public class LinkedList 
-{
-	Node head=null;
+public class Test<T> {
+	Node head = null;
 
-	//LinkedList stucture
-	class Node{
-		int data;
+	// LinkedList stucture
+	class Node {
+		T data;
 		Node next;
-		Node(int data){
+
+		Node(T data) {
 			this.data = data;
 			next = null;
 		}
 	}
 
-	//Add data in linked list.
-	public void add(int data){
-		Node newNode=new Node(data);
-		if(head==null){
-			head=newNode;
-		}
-		else{
-			Node q=head;
-			while(q.next!=null)
-				q=q.next;
-			q.next=newNode;
+	// Add data last in linked list.
+	public void addLast(T data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node q = head;
+			while (q.next != null)
+				q = q.next;
+			q.next = newNode;
 		}
 		System.out.println("Data added successfully.");
 	}
 
-	//Change value at specific index.
-	public void changeByIndex(int index,int data){
-		if(head==null){
-			System.out.println("Linked list is empty.");
+	// Add data first in linked list.
+	public void addFirst(T data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
 		}
-		else{
-			Node q=head;
+		System.out.println("Data added successfully.");
+	}
+
+	// Change value at specific index.
+	public void changeByIndex(int index, T data) {
+		if (head == null) {
+			System.out.println("Linked list is empty.");
+		} else {
+			Node q = head;
 			int i;
-			for(i=0;q!=null&&i<index;i++){
-				q=q.next;
+			for (i = 0; q != null && i < index; i++) {
+				q = q.next;
 			}
-			if(q==null || i<index)
+			if (q == null)
 				System.out.println("Not available.");
-			else{
-				q.data=data;
+			else {
+				q.data = data;
 				System.out.println("Data changed successfully.");
 			}
 		}
 	}
 
-	//Change By Data
-	public void changeByData(int oldData,int newData){
-		if(head==null){
+	// Change By Data
+	public void changeByData(T oldData, T newData) {
+		if (head == null) {
 			System.out.println("Linked list is empty.");
-		}
-		else{
-			Node q=head;
-			while(q!=null){
-				if(q.data==oldData)
-					q.data=newData;
-				q=q.next;
+		} else {
+			Node q = head;
+			while (q != null) {
+				if (q.data.equals(oldData))
+					q.data = newData;
+				q = q.next;
 			}
-			System.out.println(oldData+" data changed to "+newData+" successfully.");
+			System.out.println(oldData + " data changed to " + newData + " successfully.");
 		}
 	}
 
-	//Delete at specific index.
-	public void delete(int index){
-		if(head==null){
+	// Delete at specific index.
+	public void delete(int index) {
+		if (head == null) {
 			System.out.println("Linked list is empty.");
-		}
-		else{
-			Node q=head,pre=head;
+		} else {
+			Node q = head, pre = head;
 			int i;
 
-			for(i=1;q!=null&&i<index;i++){
-				pre=q;
-				q=q.next;
+			for (i = 1; q != null && i < index; i++) {
+				pre = q;
+				q = q.next;
 			}
-			if(q==null || i<index)
+			if (q == null || i < index)
 				System.out.println("Not available.");
-			else{
-				System.out.println(q.data+" deleted successfully.");
-				if(index==0)
-					head=head.next;
+			else {
+				System.out.println(q.data + " deleted successfully.");
+				if (index == 0)
+					head = head.next;
 				else
-					pre.next=q.next;
+					pre.next = q.next;
 			}
 		}
 	}
 
-	//Show the linked list.
-	public void display(){
-		if(head==null){
+	// Show the linked list.
+	public void display() {
+		if (head == null) {
 			System.out.println("Linked list is empty.");
-		}
-		else{
-			Node q=head;
+		} else {
+			Node q = head;
 			System.out.println("LinkedList : ");
-			while(q!=null){
-				System.out.print(q.data+" ");
-				q=q.next;
+			while (q != null) {
+				System.out.print(q.data + " ");
+				q = q.next;
 			}
 			System.out.println();
 		}
 	}
 
-	public void reverse(){
-		if(head==null){
+	public void reverse() {
+		if (head == null) {
 			System.out.println("Linked list is empty.");
-		}
-		else{
-			Node curr=head,next=null,pre=null;
-			while(curr!=null){
-				next=curr.next;
-				curr.next=pre;
-				pre=curr;
-				curr=next;
+		} else {
+			Node curr = head, next = null, pre = null;
+			while (curr != null) {
+				next = curr.next;
+				curr.next = pre;
+				pre = curr;
+				curr = next;
 			}
-			head=pre;
+			head = pre;
 			System.out.println("LinkedList is reversed.");
 		}
 	}
 
-	public static void main(String arg[])
-	{
-		LinkedList ll = new LinkedList();
-		Scanner sc=new Scanner(System.in);
+	public static void main(String arg[]) {
+		Test ll = new Test();
+		Scanner sc = new Scanner(System.in);
 
-		while(true){
-			System.out.println("1. Add the data");
-			System.out.println("2. Change by index");
-			System.out.println("3. Change by data");
-			System.out.println("4. Delete data at specific index");
-			System.out.println("5. Reverse the LL.");
-			System.out.println("6. Display LL");
-			System.out.println("7. Exit");
+		while (true) {
+			System.out.println("1. Add the data at first");
+			System.out.println("2. Add the data at last");
+			System.out.println("3. Change by index");
+			System.out.println("4. Change by data");
+			System.out.println("5. Delete data at specific index");
+			System.out.println("6. Reverse the LL.");
+			System.out.println("7. Display LL");
+			System.out.println("8. Exit");
 			System.out.print("\nEnter your choice : ");
-			int choice=sc.nextInt();
-			switch(choice){
-				case 1: System.out.print("\nData : ");
-						ll.add(sc.nextInt());
-						break;
-				case 2: System.out.print("\nIndex : ");
-						int index=sc.nextInt();
-						System.out.print("New Data : ");
-						int data=sc.nextInt();
-						ll.changeByIndex(index,data);
-						break;
-				case 3: System.out.print("\nOld Data : ");
-						int oldData=sc.nextInt();
-						System.out.print("New Data : ");
-						int newData=sc.nextInt();
-						ll.changeByData(oldData,newData);
-						break;
-				case 4: System.out.print("\nIndex : ");
-						ll.delete(sc.nextInt());
-						break;
-				case 5: ll.reverse();
-						break;
-				case 6: ll.display();
-						break;
-				case 7: System.exit(0);
-						break;
-				default: System.out.println("You enter wrong choice.");
+			int choice = sc.nextInt();
+			switch (choice) {
+				case 1:
+					System.out.print("\nData : ");
+					ll.addFirst(sc.next());
+					break;
+				case 2:
+					System.out.print("\nData : ");
+					ll.addLast(sc.next());
+					break;
+				case 3:
+					System.out.print("\nIndex : ");
+					int index = sc.nextInt();
+					System.out.print("New Data : ");
+					String data = sc.next();
+					ll.changeByIndex(index, data);
+					break;
+				case 4:
+					System.out.print("\nOld Data : ");
+					String oldData = sc.next();
+					System.out.print("New Data : ");
+					String newData = sc.next();
+					ll.changeByData(oldData, newData);
+					break;
+				case 5:
+					System.out.print("\nIndex : ");
+					ll.delete(sc.nextInt());
+					break;
+				case 6:
+					ll.reverse();
+					break;
+				case 7:
+					ll.display();
+					break;
+				case 8:
+					sc.close();
+					System.exit(0);
+					break;
+				default:
+					System.out.println("You enter wrong choice.");
 			}
 			System.out.println();
 		}
