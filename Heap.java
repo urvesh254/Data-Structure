40ce5c26d74648e7f539221e25c69350e5ccfff3
urvesh254 <urveshpatel2001@gmail.com>
2020-10-29 11:31:54 +0530

    Remove some methods.

--- Heap.java
+++ Heap.java
@@ -4,121 +4,92 @@
 
 import java.util.*;
 
-public class Heap 
-{
+public class Heap {
+	private static void heapify(int[] arr, int i, int len) {
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		int max = i;
 
-	// Normal method O(n*log n) time for convert array to Heap
-	private static void convertHeap(int[] arr){
-		int len=arr.length;
+		if (left < len && arr[max] < arr[left])
+			max = left;
+		if (right < len && arr[max] < arr[right])
+			max = right;
 
-		for(int i=len/2-1;i>0;i--){
-			int parent=(i-1)/2;
-			int min=i;
+		if (max != i) {
+			swap(arr, max, i);
 
-			while(parent>=0){
-				if(arr[min]>arr[parent]){
-					min=parent;
-				}
-				if(parent==0)
-					break;
-				parent=(parent-1)/2;
-			}
-
-			if(min!=i){
-				arr[min]=arr[min]^arr[i];
-				arr[i]=arr[min]^arr[i];
-				arr[min]=arr[min]^arr[i];
-			}
+			heapify(arr, max, len);
 		}
 	}
 
-	private static void heapify(int[] arr,int i,int len){
-		int left=2*i+1;
-		int right=2*i+2;
-		int max=i;
+	public static void heapify(int[] arr) {
+		int len = arr.length;
 
-		if(left<len && arr[max]<arr[left])
-			max=left;
-		if(right<len && arr[max]<arr[right])
-			max=right;
-
-		if(max!=i){
-			swap(arr,max,i);
-
-			heapify(arr,max,len);
-		}
-	}
-
-	public static void heapify(int[] arr){
-		int len=arr.length;
-
-		for(int i=len/2-1;i>=0;i--){
-			heapify(arr,i,len);
+		for (int i = len / 2 - 1; i >= 0; i--) {
+			heapify(arr, i, len);
 		}
 	}
 
 	// HeapSort logic
-	private static void heapSort(int[] arr){
-		// convertHeap(arr);
-		int len=arr.length;
+	private static void heapSort(int[] arr) {
+		int len = arr.length;
 
 		heapify(arr);
 
-		for(int i=len-1;i>0;i--){
-			swap(arr,0,i);
+		for (int i = len - 1; i > 0; i--) {
+			swap(arr, 0, i);
 
-			heapify(arr,0,i);
+			heapify(arr, 0, i);
 		}
 
 	}
 
-	private static void swap(int[] arr,int i,int j){
-		arr[i]=arr[i]^arr[j];
-		arr[j]=arr[i]^arr[j];
-		arr[i]=arr[i]^arr[j];
+	private static void swap(int[] arr, int i, int j) {
+		arr[i] = arr[i] ^ arr[j];
+		arr[j] = arr[i] ^ arr[j];
+		arr[i] = arr[i] ^ arr[j];
 	}
 
 	// Add element in heap
-	private static int[] add(int temp[],int data){
-		int arr[] = new int[temp.length+1];
-		int len=arr.length-1;
+	private static int[] add(int temp[], int data) {
+		int arr[] = new int[temp.length + 1];
+		int len = arr.length - 1;
 
-		for(int i=0;i<len;i++)
-			arr[i]=temp[i];
-		arr[len]=data;
+		for (int i = 0; i < len; i++)
+			arr[i] = temp[i];
+		arr[len] = data;
 
-		int parent=(len-1)/2;
-		int min=len;
+		int parent = (len - 1) / 2;
+		int min = len;
 
-		while(parent>=0){
-			if(arr[min]>arr[parent]){
-				arr[min]=arr[min]^arr[parent];
-				arr[parent]=arr[min]^arr[parent];
-				arr[min]=arr[min]^arr[parent];
-				min=parent;
-			}else{
+		while (parent >= 0) {
+			if (arr[min] > arr[parent]) {
+				arr[min] = arr[min] ^ arr[parent];
+				arr[parent] = arr[min] ^ arr[parent];
+				arr[min] = arr[min] ^ arr[parent];
+				min = parent;
+			} else {
 				break;
 			}
 
-			if(parent==0)
+			if (parent == 0)
 				break;
-			parent=(parent-1)/2;
+			parent = (parent - 1) / 2;
 		}
 
 		return arr;
 	}
 
 	// Delete element from heap
-	private static int[] delete(int arr[]){
-		int len=arr.length-1;
-		int i=0,left,right,min=0;
-		
-		arr[len]=arr[len]^arr[0];
-		arr[0]=arr[len]^arr[0];
-		arr[len]=arr[len]^arr[0];
+	private static int[] delete(int arr[]) {
+		int len = arr.length - 1;
 
-		heapify(arr,0,len);
+		arr[len] = arr[len] ^ arr[0];
+		arr[0] = arr[len] ^ arr[0];
+		arr[len] = arr[len] ^ arr[0];
 
-		return Arrays.copyOfRange(arr,0,arr.length-1);
+		heapify(arr, 0, len);
+
+		return Arrays.copyOfRange(arr, 0, arr.length - 1);
 	}
 }
